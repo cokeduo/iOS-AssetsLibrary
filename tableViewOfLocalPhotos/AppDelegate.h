b13235ed7987cf0d655ed456ada469e831d50087
王铎 <cokeduo@163.com>
2016-03-15 09:28:39 +0800

    Initial Commit

---
+++ tableViewOfLocalPhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableViewOfLocalPhotos
+//
+//  Created by change009 on 16/3/15.
+//  Copyright © 2016年 change009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
